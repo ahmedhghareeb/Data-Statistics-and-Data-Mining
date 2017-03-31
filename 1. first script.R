d1123d62ad2b3b6c9620cec1b0f10a934a2bdc2d
ahmedxtb <ahmedxtb@gmail.com>
2017-03-31 19:42:59 -0400

    Add files via upload

---
+++ 1. first script.R
@@ -0,0 +1,13 @@
+a = 2
+b =2 
+
+b <- 2
+
+library(googleVis)
+df=data.frame(country=c("US", "GB", "BR"), 
+              val1=c(10,13,14), 
+              val2=c(23,12,32))
+
+Bar <- gvisBarChart(df)
+plot(Bar)
+####
